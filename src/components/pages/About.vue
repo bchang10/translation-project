e9064cccdde7bef06465dd6d5ffb22a8bcc769a1
LRIPZ <noulong3gmail.com>
2024-09-18 23:52:35 -0500

    done

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <h1>Our Mission/About Us/Our Goal/Our History</h1>
+    <h1>About Us</h1>
     <p> We're Brendan, Yee, and Nou, the team behind Hmong <br>
         Translation. Brendan, who works at Sentry, originally <br>
         started this project as a fun coding experiment. <br>
@@ -12,6 +12,7 @@
         you will find us playing video games, <br>
         enjoying disc golf, or caring for our beloved dogs. <br>
         <br>
+        <h2>Our Mission</h2>
         We started this journey to inspire those interested in learning Hmong or simply looking to translate <br>
         and better understand the language. We hope you are excited to begin your Hmong journey with us!
     </p>
